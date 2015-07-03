96085a6cd638baebe43d32efd225cb6642a23837
Andy Stratton <theandystratton@gmial.com>
2015-07-02 20:24:44 -0400

    Comment updates.

--- referer-spam-blocker.php
+++ referer-spam-blocker.php
@@ -4,6 +4,8 @@ Plugin Name: Referer Spam Blocker
 Description: Blocks most common HTTP referer spam domains and allows you to easily add custom domains.
 Author: WP Maintainer
 Author URI: http://wpmaintainer.com
+Version: 0.1
+License: GPLv2 or later
 */
 foreach ( glob( dirname( __FILE__ ) . '/lib/*.php' ) as $file )
 {
