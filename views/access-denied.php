2fbe58c8b15e90a0abc3c5160dc92342a2894f34
Andy Stratton <theandystratton@gmial.com>
2015-07-08 18:16:18 -0400

    Remove

--- views/access-denied.php
+++ views/access-denied.php
@@ -11,10 +11,7 @@
 
 	<h1>You are forbidden to access this page.</h1>
 	<p>Based on your referring domain, you have been disallowed access to this website.</p>
-	<p>
-		<em>Powered by <a href="http://wpmaintainer.com/referer-spam-blocker">Referer Spam Blocker</a>
-		by <a href="http://wpmaintainer.com">WP Maintainer</a></em>
-	</p>
+
 </center>
 </body>
 </html>
